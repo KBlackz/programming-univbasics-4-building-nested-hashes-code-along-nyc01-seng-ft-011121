4164201f5a009224fabc3c807673ed37d9716a29
Karris  Black <karris.b@gmail.com>
2021-01-02 17:35:22 +0000

    Automatically backed up by Learn

--- spec/04_challenge_spec.rb
+++ spec/04_challenge_spec.rb
@@ -16,10 +16,3 @@ end
 end
 
 
-:capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}]
-    }
